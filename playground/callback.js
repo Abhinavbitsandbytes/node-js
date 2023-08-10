60ed2044682db90cabc5025ad28e1a3a7fd60a99
abhinavbitsandbytes <abhinavkr197@gmail.com>
2023-08-10 15:32:16 +0530

    just before seperate route file video and auth module

--- playground/callback.js
+++ playground/callback.js
@@ -1,23 +1,18 @@
-const doWorkCallback = (callback) => {
-    setTimeout(() =>{
-        callback(undefined, [1,4,7])
-    }, 2000)
-}
-doWorkCallback((error, result) => {
-    if(error){
-        return console.log(error);
-    }
-    console.log(result);
-})
-
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(()=>{
-       reject('Things went wrong') 
+const add = (a,b) =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve(a+b);
+        }, 2000)
     })
-})
-
-doWorkPromise.then((result)=>{
-console.log(result);
-}).catch((error)=>{
-    console.log('error', error)
+}
+const doWork = async () =>{
+const sum = await add(1,99)
+const sum2 = await add(sum,50)
+const sum3 = await add(sum2,3)
+return sum3
+}
+doWork().then((result)=>{
+    console.log('result', result)
+}).catch((e)=>{
+    console.log(e)
 })
